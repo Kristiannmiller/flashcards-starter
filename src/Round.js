6eebfe039843c5dc336e0f44b26dc1f5fa78f32d
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-19 18:26:59 -0600

    Create returnCurrentCard method

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,10 @@
 class Round {
-  constructor() {
-
+  constructor(deck) {
+    this.deck = deck;
+    this.turn = 0;
+  };
+  returnCurrentCard() {
+    return this.deck.cards[this.turn]
   }
-}
+};
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,18 +1,29 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Round = require('../src/Round');
+const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Round = require('../src/Round');
 
 describe('Round', () => {
 
   it('should be a function', () => {
-    const round = new Round();
     expect(Round).to.be.a('function');
   });
 
-  it.skip('should be an instance of Round', () => {
+  it('should be an instance of Round', () => {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
+  });
+
+  it('should know the current card', () => {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card3 = new Card(3, 'What kind of bear is best?', ['panda bear', 'balck bear', 'that\'s a ridiculous question'], 'black bear');
+
+    const deck = new Deck([card1, card2, card3])
+    const round = new Round(deck);
+    expect(round.returnCurrentCard()).to.equal(deck.cards[0]);
   });
 });
