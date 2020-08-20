b7a5d9e36164f0fa6a841977b6bb0420e5ae2907
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-19 19:40:41 -0600

    Add Turn instantiation to takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -1,12 +1,17 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck) {
     this.deck = deck;
     this.turn = 0;
+    this.currentTurn = {};
   };
   returnCurrentCard() {
     return this.deck.cards[this.turn]
   }
-  takeTurn() {
+  takeTurn(guess) {
+    const currentTurn = new Turn(guess, this.deck.cards[this.turn])
+    this.currentTurn = currentTurn
     this.turn++
   }
 };
